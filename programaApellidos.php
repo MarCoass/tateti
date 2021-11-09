21162caccb3d2f2b15f3e615909a3be6f28638d2
Hakaiq <93964777+Hakaiq@users.noreply.github.com>
2021-11-09 20:14:06 -0300

    sea

--- programaApellidos.php
+++ programaApellidos.php
@@ -21,6 +21,8 @@ include_once("tateti.php");
  * ¿podria ser que retorne el echo del final al programa principal? ¿esta bien que sea una function o ponemos el codigo directamente
  * en el case?
  */
+
+
 function mostrarJuegoPorNumero($coleccionJuegos , $cantJuegos)
 {
     //Se solicita un numero de juego y se muestra la informacion de dicho juego
@@ -51,6 +53,33 @@ function mostrarJuegoPorNumero($coleccionJuegos , $cantJuegos)
     Jugador O: " . $juegoBuscado["jugadorCirculo"] . " obtuvo " . $juegoBuscado["puntosCirculo"] . "puntos.\n";
 }
 
+function cargarJuegos(){
+
+    $coleccionJuegos[0] = ["jugadorCruz"=> "pipo" , "jugadorCirculo" => "alex", "puntosCruz"=> 8, "puntosCirculo" => 3];
+    $coleccionJuegos[1] = ["jugadorCruz"=> "alex" , "jugadorCirculo" => "jose", "puntosCruz"=> 5, "puntosCirculo" => 0];
+    $coleccionJuegos[2] = ["jugadorCruz"=> "yiyo" , "jugadorCirculo" => "franco", "puntosCruz"=> 2, "puntosCirculo" => 3];
+    $coleccionJuegos[3] = ["jugadorCruz"=> "tomas" , "jugadorCirculo" => "fernando", "puntosCruz"=> 4, "puntosCirculo" => 1];
+    $coleccionJuegos[4] = ["jugadorCruz"=> "matias" , "jugadorCirculo" => "fer", "puntosCruz"=> 2, "puntosCirculo" => 6];
+    $coleccionJuegos[5] = ["jugadorCruz"=> "majo" , "jugadorCirculo" => "yone", "puntosCruz"=> 3, "puntosCirculo" => 1];
+    $coleccionJuegos[6] = ["jugadorCruz"=> "alberto" , "jugadorCirculo" => "tata", "puntosCruz"=> 0, "puntosCirculo" => 7];
+    $coleccionJuegos[7] = ["jugadorCruz"=> "yiyo" , "jugadorCirculo" => "sona", "puntosCruz"=> 3, "puntosCirculo" => 3];
+    $coleccionJuegos[8] = ["jugadorCruz"=> "alex" , "jugadorCirculo" => "pancho", "puntosCruz"=> 3, "puntosCirculo" => 4];
+    $coleccionJuegos[9] = ["jugadorCruz"=> "raul" , "jugadorCirculo" => "nacho", "puntosCruz"=> 6, "puntosCirculo" => 1];
+    $coleccionJuegos[10] = ["jugadorCruz"=> "tomas" , "jugadorCirculo" => "mar", "puntosCruz"=> 3, "puntosCirculo" => 4];
+    $coleccionJuegos[11] = ["jugadorCruz"=> "pipo" , "jugadorCirculo" => "ander", "puntosCruz"=> 1, "puntosCirculo" => 1];
+    $coleccionJuegos[12] = ["jugadorCruz"=> "jose" , "jugadorCirculo" => "franco", "puntosCruz"=> 4, "puntosCirculo" => 0];
+    $coleccionJuegos[13] = ["jugadorCruz"=> "paty" , "jugadorCirculo" => "mel", "puntosCruz"=> 2, "puntosCirculo" => 2];
+    $coleccionJuegos[14] = ["jugadorCruz"=> "nacho" , "jugadorCirculo" => "mauro", "puntosCruz"=> 2, "puntosCirculo" => 4];
+    
+return $coleccionJuegos;
+
+}
+
+function juegoGanador($primerGanador){
+
+
+
+}
 
 /**************************************/
 /*********** PROGRAMA PRINCIPAL *******/
@@ -62,14 +91,14 @@ $coleccionJuegos = array(); //array para almacenar la informacion de los juegos
 
 
 //Inicialización de variables:
-
+$coleccionJuegos = cargarJuegos();
 $cantJuegos = count($coleccionJuegos); //variable que almacena la cantidad de elementos del array de juegos para saber en que indice insertar el nuevo
 //Proceso:
 
 
 //print_r($juego);
 
-
+print_r ($coleccionJuegos);
 
 
 do {
